072ba5f00e0c13e3114c5afca9c76c57eaad6d0e
nicolas-soler-2022 <nicolasjs2011@hotmail.com>
2023-06-06 21:38:58 -0300

    agregamos el while en forma creciente

--- while.php
+++ while.php
@@ -1,13 +1,13 @@
 <?php
 
-/*while (0==1) {
+while (0==1) {
     echo  "while en sumando :".PHP_EOL;
 }
 $contador =  1;
 while($contador <=11){
     echo " forma creciente :  $contador".PHP_EOL;
     $contador++;
-}*/
+}
 while 
 (0==1) {
     echo  "while en decreciente :".PHP_EOL;}
